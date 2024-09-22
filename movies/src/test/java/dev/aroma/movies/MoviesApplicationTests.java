9a4775a24d7c2e0a366f4f4e2bbbf8ccef7ef958
AromaLucas <85728999+AromaLucas@users.noreply.github.com>
2024-09-22 21:26:12 +0100

    Add files via upload

---
+++ movies/src/test/java/dev/aroma/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aroma.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
